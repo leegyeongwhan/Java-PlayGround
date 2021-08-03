a4d5b8c832f4932f6ae79111430ec752f7bf7d30
leekh <leekh@DESKTOP-LE2UBGC>
2021-08-04 04:25:52 +0900

    getmaxDaysOfMonth, printCalendar

--- src/calenda/Calendar.java
+++ src/calenda/Calendar.java
@@ -2,10 +2,14 @@ package calenda;
 
 import java.util.Scanner;
 
-public class Calenda {
+public class Calendar {
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }; // 월마다 일수
 
-	public static void main(String[] args) {
+	public  int getmaxDaysOfMonth(int month) { //
+		return MAX_DAYS[month-1]; 
+	}
 
+	public void printCalendar() {
 		System.out.println("일 월 화 수 목 금 토 일");
 		System.out.println("------------------");
 		for (int i = 1; i < 29; i++) {
@@ -15,14 +19,16 @@ public class Calenda {
 			}
 		}
 
+	}
+	public static void main(String[] args) {		
 		Scanner scanner = new Scanner(System.in);
+		Calendar cal = new Calendar();
 		System.out.println("달을 입력해주세요");
 		// String dal = scanner.next();
 		// int month = Integer.parseInt(dal);
 		int month = scanner.nextInt(); // 달
-		int[] maxDays = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-		System.out.printf(" %d 월은 %d일 까지있습니다 \n", month, maxDays[month - 1]);
+		System.out.printf(" %d 월은 %d일 까지있습니다 \n", month, cal.getmaxDaysOfMonth(month));
 		scanner.close();
 	}
 
